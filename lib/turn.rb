3ed0693005de17733f3a290fb2fec98066c2c3c3
Christopher Dent <christopherdent01@gmail.com>
2019-06-29 03:41:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,6 @@
 
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
-  puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
-  puts ("-----------")
-  puts (" #{board[3]} | #{board[4]} | #{board[5]} ")
-  puts ("-----------")
-  puts (" #{board[6]} | #{board[7]} | #{board[8]} ")
-end
-
 
 def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0, 8)
@@ -28,15 +20,21 @@ def position_taken?(board, index)
 end 
 
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end 
 
-
 def move(board, user_input, value = "X")
  board[user_input]=value
 end
-
 
 def turn(board)
   puts "Please enter 1-9:"
